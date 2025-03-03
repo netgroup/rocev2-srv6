458e70f3e4fae7b30dafe06b4c58656256424e48
Paolo Lungaroni <paolo.lungaroni@uniroma2.it>
2025-03-03 03:14:15 +0100

    Add new router into testbed topology
    This commit adds a new router rt0 in the testbed topology.
    In this context, a refactoring of the updated names r0 and
    r1 was done in gw1 and gw2 respectively.
    
    Signed-off-by: Paolo Lungaroni <paolo.lungaroni@uniroma2.it>

--- tests/encap_decap_test.sh
+++ tests/encap_decap_test.sh
@@ -3,19 +3,19 @@
 #                set CE on outer IPv6 traffic class
 #  enable ECT bit                +
 #      +                         |
-#      |              +----------|---+     +--------------+
-#      v              |      r0  |   |     |      r1      |
-#  +-------+          |          |   |     |              |        +-------+
-#  |   h0  |          |          v   |     |              |        |  h1   |
-#  |       +----------+veth1    veth2+-----+veth3    veth4+--------+       |
-#  | veth0 |          |  ^       ^   |     |  ^       ^   |        | veth5 |
-#  +-------+          |  |       |   |     |  |       |   |        +-------+
-#                     |  |       |   |     |  |       |   |
-# 10.0.1.1/24         +--|-------|---+     +--|-------|---+       10.0.2.1/24
-#                        |       |            |       |
-#                        +       +            +       +
-#                   eBPF/XDP   eBPF/XDP   ePF/XDP   eBPF/XDP
-#                    encap      decap      decap     encap
+#      |              +----------|---+     +--------------+     +--------------+
+#      v              |      gw1 |   |     |      rt0     |     |      gw2     |
+#  +-------+          |          |   |     |              |     |              |        +-------+
+#  |   h0  |          |          v   |     |              |     |              |        |  h1   |
+#  |       +----------+veth1    veth2+-----+veth3    veth4+-----+veth5    veth6+--------+       |
+#  | veth0 |          |  ^       ^   |     |  ^       ^   |     |  ^       ^   |        | veth7 |
+#  +-------+          |  |  (a1) |   |     |  |  (b0) |   |     |  |  (a2) |   |        +-------+
+#                     |  |       |   |     |  |       |   |     |  |       |   |
+# 10.0.1.1/24         +--|-------|---+     +--|-------|---+     +--|-------|---+       10.0.2.1/24
+#                        |       |            |       |            |       |
+#                        +       +            +       +            +       +
+#                   eBPF/XDP   eBPF/XDP  eBPF/XDP   eBPF/XDP  eBPF/XDP   eBPF/XDP
+#                    encap      decap     pass       pass      decap      encap
 #
 #
 
@@ -43,12 +43,15 @@ ip -all netns delete
 ip netns add h0
 ip netns add h1
 
-ip netns add r0
-ip netns add r1
+ip netns add gw1
+ip netns add rt0
+ip netns add gw2
 
-ip link add veth0 netns h0 type veth peer name veth1 netns r0
-ip link add veth2 netns r0 type veth peer name veth3 netns r1
-ip link add veth4 netns r1 type veth peer name veth5 netns h1
+ip link add veth0 netns h0 type veth peer name veth1 netns gw1
+ip link add veth2 netns gw1 type veth peer name veth3 netns rt0
+ip link add veth4 netns rt0 type veth peer name veth5 netns gw2
+ip link add veth6 netns gw2 type veth peer name veth7 netns h1
+
 
 ###################
 #### Node: h0 #####
@@ -88,10 +91,11 @@ read -r -d '' h0_env <<-EOF
 EOF
 set -e
 
-###################
-#### Node: r0 #####
-###################
-NODE=r0
+
+####################
+#### Node: gw1 #####
+####################
+NODE=gw1
 echo -e "\nNode: $NODE"
 ip netns exec $NODE sysctl -w net.ipv4.conf.all.forwarding=1
 ip netns exec $NODE sysctl -w net.ipv6.conf.all.forwarding=1
@@ -107,7 +111,7 @@ ip netns exec $NODE ip link set dev lo up
 ip netns exec $NODE ip link set dev veth1 up
 ip netns exec $NODE ip link set dev veth2 up
 ip netns exec $NODE ip addr add 10.0.1.254/24 dev veth1
-ip netns exec $NODE ip addr add fd00:0:1::1/48 dev veth2
+ip netns exec $NODE ip addr add fd00:a1:b0::1/48 dev veth2
 
 if [ "$DEBUG" == "on" ]; then
 	# SRv6 microSID Encap (for DEBUG only)
@@ -120,14 +124,14 @@ if [ "$DEBUG" == "on" ]; then
 fi
 
 # Forward Routing
-ip netns exec $NODE ip -6 route add fc00:0::/32 via fd00:0:1::2 dev veth2
+ip netns exec $NODE ip -6 route add fc00:0::/32 via fd00:a1:b0::2 dev veth2
 
 # ip6tables rule to mark ECT+CE traffic
 ip netns exec $NODE ip6tables \
 	-t mangle -A POSTROUTING -d fc00:0:512:: -j TOS --set-tos 0x03/0xff
 
 set +e
-read -r -d '' r0_env <<-EOF
+read -r -d '' gw1_env <<-EOF
 	mount -t bpf bpf "${BPFFS_PATH}"
 	mount -t tracefs nodev /sys/kernel/tracing
 
@@ -165,23 +169,76 @@ read -r -d '' r0_env <<-EOF
 	# key:
 	# 	0-3:	ipv4 dst address		(10.0.2.1)
 	# value:
-	# 	0-7:	ipv6 Source tunnel address	(fd00:0:1::1)
+	# 	0-7:	ipv6 Source tunnel address	(fd00:a1:b0::1)
 	# 	8-15:	ipv6 Destination uSID carrier	(fc00:0:0512::)
 	${BPFTOOL} \
 		map update \
 		pinned "${BPFFS_PATH}/maps/sr6encap_ip4_table"			\
 	        key hex         01 02 00 0a                                     \
-	        value hex       fd 00 00 00 00 01 00 00 00 00 00 00 00 00 00 01 \
+	        value hex       fd 00 00 a1 00 b0 00 00 00 00 00 00 00 00 00 01 \
 	                        fc 00 00 00 05 12 00 00 00 00 00 00 00 00 00 00
 
 	/bin/bash
 EOF
 set -e
 
-###################
-#### Node: r1 #####
-###################
-NODE=r1
+
+####################
+#### Node: rt0 #####
+####################
+NODE=rt0
+echo -e "\nNode: $NODE"
+ip netns exec $NODE sysctl -w net.ipv6.conf.all.forwarding=1
+
+ip netns exec $NODE ip link set dev lo up
+ip netns exec $NODE ip link set dev veth3 up
+ip netns exec $NODE ip link set dev veth4 up
+ip netns exec $NODE ip addr add fd00:a1:b0::2/48 dev veth3
+ip netns exec $NODE ip addr add fd00:b0:a2::1/48 dev veth4
+
+if [ "$DEBUG" == "on" ]; then
+	# Actually do nothing.
+	:
+fi
+
+# Forward Routing
+ip netns exec $NODE ip -6 route add fc00:0::/32 via fd00:b0:a2::2 dev veth4
+
+# Reverse Routing
+ip netns exec $NODE ip -6 route add fc01:0::/32 via fd00:a1:b0::1 dev veth3
+
+set +e
+read -r -d '' rt0_env <<-EOF
+	mount -t bpf bpf "${BPFFS_PATH}"
+	mount -t tracefs nodev /sys/kernel/tracing
+
+	# It allows to load maps with many entries without failing
+	ulimit -l unlimited
+
+	mkdir "${BPFFS_PATH}"/{progs,maps}
+
+	${BPFTOOL} prog loadall \
+		"${OBJ_PATH}/xdp.bpf.o" /sys/fs/bpf/progs \
+		pinmaps /sys/fs/bpf/maps \
+		type xdp
+
+	${BPFTOOL} net attach xdpdrv \
+		pinned "${BPFFS_PATH}/progs/xdp_pass" \
+		dev veth3
+
+	${BPFTOOL} net attach xdpdrv \
+		pinned "${BPFFS_PATH}/progs/xdp_pass" \
+		dev veth4
+
+	/bin/bash
+EOF
+set -e
+
+
+####################
+#### Node: gw2 #####
+####################
+NODE=gw2
 echo -e "\nNode: $NODE"
 ip netns exec $NODE sysctl -w net.ipv4.conf.all.forwarding=1
 ip netns exec $NODE sysctl -w net.ipv6.conf.all.forwarding=1
@@ -190,30 +247,30 @@ ip netns exec $NODE sysctl -w net.ipv6.conf.all.forwarding=1
 ip netns exec $NODE sysctl -w net.ipv4.conf.all.rp_filter=0
 ip netns exec $NODE sysctl -w net.ipv4.conf.lo.rp_filter=0
 ip netns exec $NODE sysctl -w net.ipv4.conf.default.rp_filter=0
-ip netns exec $NODE sysctl -w net.ipv4.conf.veth3.rp_filter=0
-ip netns exec $NODE sysctl -w net.ipv4.conf.veth4.rp_filter=0
+ip netns exec $NODE sysctl -w net.ipv4.conf.veth5.rp_filter=0
+ip netns exec $NODE sysctl -w net.ipv4.conf.veth6.rp_filter=0
 
 ip netns exec $NODE ip link set dev lo up
-ip netns exec $NODE ip link set dev veth3 up
-ip netns exec $NODE ip link set dev veth4 up
-ip netns exec $NODE ip addr add fd00:0:1::2/48 dev veth3
-ip netns exec $NODE ip addr add 10.0.2.254/24 dev veth4
+ip netns exec $NODE ip link set dev veth5 up
+ip netns exec $NODE ip link set dev veth6 up
+ip netns exec $NODE ip addr add fd00:b0:a2::2/48 dev veth5
+ip netns exec $NODE ip addr add 10.0.2.254/24 dev veth6
 
 if [ "$DEBUG" == "on" ]; then
 	# SRv6 microSID Encap (for DEBUG only)
 	ip netns exec $NODE ip -4 route add 10.0.1.1/32 \
-			encap seg6 mode encap.red segs fc01:0:0512:: dev veth4
+			encap seg6 mode encap.red segs fc01:0:0512:: dev veth6
 
 	# SRv6 microSID Decap (for DEBUG only)
 	ip netns exec $NODE ip -6 route add fc00:0:0512::/128 \
-		encap seg6local action End.DX4 nh4 0.0.0.0 dev veth3
+		encap seg6local action End.DX4 nh4 0.0.0.0 dev veth5
 fi
 
 # Reverse Routing
-ip netns exec $NODE ip -6 route add fc01:0::/32 via fd00:0:1::1 dev veth3
+ip netns exec $NODE ip -6 route add fc01:0::/32 via fd00:b0:a2::1 dev veth5
 
 set +e
-read -r -d '' r1_env <<-EOF
+read -r -d '' gw2_env <<-EOF
 	mount -t bpf bpf "${BPFFS_PATH}"
 	mount -t tracefs nodev /sys/kernel/tracing
 
@@ -229,7 +286,7 @@ read -r -d '' r1_env <<-EOF
 
 	${BPFTOOL} net attach xdpdrv \
 		pinned "${BPFFS_PATH}/progs/xdp_sr6decap" \
-		dev veth3
+		dev veth5
 
 	# decap SID (in bytes)
 	# key:
@@ -244,24 +301,25 @@ read -r -d '' r1_env <<-EOF
 
 	${BPFTOOL} net attach xdpdrv \
 		pinned "${BPFFS_PATH}/progs/xdp_sr6encap" \
-		dev veth4
+		dev veth6
 
 	# encap policy (in bytes)
 	# key:
 	# 	0-3:	ipv4 dst address		(10.0.1.1)
 	# value:
-	# 	0-15:	ipv6 Source tunnel address	(fd00:0:1::2)
+	# 	0-15:	ipv6 Source tunnel address	(fd00:b0:a2::2)
 	# 	16-31:	ipv6 Destination uSID carrier	(fc01:0:0512::)
 	${BPFTOOL} \
 		map update \
 		pinned "${BPFFS_PATH}/maps/sr6encap_ip4_table"			\
 	        key hex         01 01 00 0a                                     \
-	        value hex       fd 00 00 00 00 01 00 00 00 00 00 00 00 00 00 02 \
+	        value hex       fd 00 00 b0 00 a2 00 00 00 00 00 00 00 00 00 02 \
 	                        fc 01 00 00 05 12 00 00 00 00 00 00 00 00 00 00
 
 	/bin/bash
 EOF
 set -e
+
 
 ###################
 #### Node: h1 #####
@@ -269,9 +327,9 @@ set -e
 NODE=h1
 echo -e "\nNode: $NODE"
 ip netns exec $NODE ip link set dev lo up
-ip netns exec $NODE ip link set dev veth5 up
-ip netns exec $NODE ip addr add 10.0.2.1/24 dev veth5
-ip netns exec $NODE ip -4 route add 10.0.1.0/24 via 10.0.2.254 dev veth5
+ip netns exec $NODE ip link set dev veth7 up
+ip netns exec $NODE ip addr add 10.0.2.1/24 dev veth7
+ip netns exec $NODE ip -4 route add 10.0.1.0/24 via 10.0.2.254 dev veth7
 
 set +e
 read -r -d '' h1_env <<-EOF
@@ -290,7 +348,7 @@ read -r -d '' h1_env <<-EOF
 
 	${BPFTOOL} net attach xdpdrv \
 		pinned "${BPFFS_PATH}/progs/xdp_pass" \
-		dev veth5
+		dev veth7
 
 	/bin/bash
 EOF
@@ -301,8 +359,9 @@ set -e
 ## Create a new tmux session ##
 ###############################
 tmux new-session -d -s $TMUX -n h0 ip netns exec h0 bash -c "${h0_env}"
-tmux new-window -t $TMUX -n r0 ip netns exec r0 bash -c "${r0_env}"
-tmux new-window -t $TMUX -n r1 ip netns exec r1 bash -c "${r1_env}"
+tmux new-window -t $TMUX -n gw1 ip netns exec gw1 bash -c "${gw1_env}"
+tmux new-window -t $TMUX -n rt0 ip netns exec rt0 bash -c "${rt0_env}"
+tmux new-window -t $TMUX -n gw2 ip netns exec gw2 bash -c "${gw2_env}"
 tmux new-window -t $TMUX -n h1 ip netns exec h1 bash -c "${h1_env}"
 tmux set-option -g mouse on
 tmux select-window -t :0
